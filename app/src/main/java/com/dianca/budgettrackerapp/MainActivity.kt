d0de33fa8fd7c74eeb17f43d98a9c7742ddee715
Dianca Jade Naidu <ST10261874@vcconnect.edu.za>
2025-04-13 15:00:49 +0200

    Added MainActivity and activity_main

--- app/src/main/java/com/dianca/budgettrackerapp/MainActivity.kt
+++ app/src/main/java/com/dianca/budgettrackerapp/MainActivity.kt
@@ -2,20 +2,23 @@ package com.dianca.budgettrackerapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.dianca.budgettrackerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val btnGoToLogin = findViewById<Button>(R.id.btnGoToLogin)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btnGoToLogin.setOnClickListener {
-            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        // Start button -> directs to login screen
+        binding.StartButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
