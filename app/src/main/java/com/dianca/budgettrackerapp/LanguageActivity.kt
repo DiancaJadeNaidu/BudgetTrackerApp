fd426314d7589558cb6c77bf1e7896d7d11c22ac
Dianca Jade Naidu <ST10261874@vcconnect.edu.za>
2025-04-13 16:05:44 +0200

    Connected to Firebase

--- app/src/main/java/com/dianca/budgettrackerapp/LanguageActivity.kt
+++ app/src/main/java/com/dianca/budgettrackerapp/LanguageActivity.kt
@@ -1,29 +1,29 @@
 package com.dianca.budgettrackerapp
-
-import android.os.Bundle
-import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.ListView
-import androidx.appcompat.app.AppCompatActivity
-
-class LanguageActivity : AppCompatActivity() {
-    private var languageListView: ListView? = null
-    private var saveLanguageButton: Button? = null
-    private var languages: Array<String> = arrayOf("English", "Spanish", "French", "German", "Chinese", "Japanese")
-    private var languageAdapter: ArrayAdapter<String>? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.language_activity)
-
-        languageListView = findViewById(R.id.language_list)
-        saveLanguageButton = findViewById(R.id.save_language_button)
-
-        languageAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, languages)
-        languageListView?.adapter = languageAdapter
-
-        saveLanguageButton?.setOnClickListener { view: View? ->
-        }
-    }
-}
+//
+//import android.os.Bundle
+//import android.view.View
+//import android.widget.ArrayAdapter
+//import android.widget.Button
+//import android.widget.ListView
+//import androidx.appcompat.app.AppCompatActivity
+//
+//class LanguageActivity : AppCompatActivity() {
+//    private var languageListView: ListView? = null
+//    private var saveLanguageButton: Button? = null
+//    private var languages: Array<String> = arrayOf("English", "Spanish", "French", "German", "Chinese", "Japanese")
+//    private var languageAdapter: ArrayAdapter<String>? = null
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.language_activity)
+//
+//        languageListView = findViewById(R.id.language_list)
+//        saveLanguageButton = findViewById(R.id.save_language_button)
+//
+//        languageAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, languages)
+//        languageListView?.adapter = languageAdapter
+//
+//        saveLanguageButton?.setOnClickListener { view: View? ->
+//        }
+//    }
+//}
